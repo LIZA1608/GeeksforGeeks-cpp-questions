facfa3cac48432d22eca47314c45096bb680a3ec
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-12-27 21:36:52 +0530

    Create Kth smallest element.cpp

---
+++ Kth smallest element.cpp
@@ -0,0 +1,33 @@
+/*https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1*/
+
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        
+        priority_queue<int>pq;
+        for(int i=0;i<k;i++){
+            pq.push(arr[i]);
+        }
+        for(int i=k;i<=r;i++){
+            if(arr[i]<pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        int ans=pq.top();
+        return ans;
+    }
+};
