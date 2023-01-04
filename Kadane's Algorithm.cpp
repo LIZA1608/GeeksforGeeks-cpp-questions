3888d151242dfdddffe985254f1eee721ea39868
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-01-05 00:36:28 +0530

    Create Kadane's Algorithm.cpp

---
+++ Kadane's Algorithm.cpp
@@ -0,0 +1,21 @@
+
+/*https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1*/
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        long long maxSum=INT_MIN;
+        long long currSum=0;
+        for(int i=0;i<n;i++){
+            currSum+=arr[i];
+            maxSum=max(maxSum,currSum);
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return maxSum;
+    }
+};
